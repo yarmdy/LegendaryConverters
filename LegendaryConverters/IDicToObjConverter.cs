e6c4ecd466051d68e1fdade5891efac45b4e4547
yarmdy <jxy2000121@sina.com>
2025-03-26 10:25:09 +0800

    添加项目文件。

---
+++ LegendaryConverters/IDicToObjConverter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace LegendaryConverters
+{
+    public interface IDicToObjConverter
+    {
+        T Convert<T>(IDictionary<string, object?> dic) where T : class, new();
+    }
+}
